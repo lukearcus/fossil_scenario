e20ea4bd89a7ea14a0cf1402b9a0c3dca1705a2a
lukearcus <rickard@robots.ox.ac.uk>
2024-09-09 17:12:41 +0100

    New models

--- Nejati23/main.py
+++ Nejati23/main.py
@@ -1,6 +1,24 @@
+import cvxpy as cp
+import numpy as np
 
+def Jet_engine(N):
+    c = 0
+    L_f = 4.71
+    M_f = 3.16
+    max_rhoP = 0.6
+    L_g = 8.06
+    lambd = 3.1
+    beta = 0.01
 
 class RCP_SCP:
-    def __init__(self, data, barrier_struct, data_params):
-        self.data = data
+    def __init__(self, barrier_func, barrier_lie_func, barr_data_params):
+        self.barr = barrier_func
+        self.barr_lie = barrier_lie_func
+        self.data_params = barr_data_params
+
+    def solve(self, data):
+        self.data_params.value = data
+        gamma = cp.Variable(1)
+        lambd = cp.Variable(1)
+        c = cp.Variable(1)
         

60ad80f0f3917415fa33425dc7c46e820e152c03
lukearcus <rickard@robots.ox.ac.uk>
2025-08-22 14:33:32 +0100

    300 samples

--- experiments/test_control.py
+++ experiments/test_control.py
@@ -48,7 +48,7 @@ def solve(systems, sets, n_data, activations, hidden_neurons, data):
 
 
 def test_lnn():
-    n_data = 3
+    n_data = 300
     system = models.LTI_disc_param 
     
     def random_control(obj, t, x):

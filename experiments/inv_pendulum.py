d21dfdbe9868f30804f3e7e84345dafcedbb2f33
Luke Rickard <rickard@robots.ox.ac.uk>
2026-09-08 20:45:33 +0100

    Revert "failed again"
    This reverts commit ca402bccab3e3ff519c2090e01934085d74a5611.

--- experiments/inv_pendulum.py
+++ experiments/inv_pendulum.py
@@ -70,7 +70,7 @@ def solve(systems, sets, n_data, activations, hidden_neurons, data):
 
 
 def test_lnn():
-    n_data = 10
+    n_data = 100
     system = models.InvPendulum
     
     def random_control(obj, t, x):

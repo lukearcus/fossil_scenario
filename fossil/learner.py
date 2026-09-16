d4694c45217857031a02f71384d5e0c466ba0b2c
Luke Rickard <rickard@robots.ox.ac.uk>
2026-09-16 07:52:01 +0100

    Tanh back

--- fossil/learner.py
+++ fossil/learner.py
@@ -855,6 +855,8 @@ class Controller(LearnerNN):
             y = activation(self.acts[idx], z)
 
         y = self.layers[-1](y)
+        tanh = torch.nn.Tanh()
+        y = tanh(y) * (self.u_max - self.u_min) / 2 + (self.u_max + self.u_min) / 2
         return y
     
     def nn_dot(self, S: torch.Tensor, Sdot: torch.Tensor, times: torch.Tensor) -> torch.Tensor:
